b00f5f2358aa7281c6beb0092264680ec996cc86
CesarMatus <46377900+CesarMatus@users.noreply.github.com>
2019-06-04 17:16:16 -0400

    Initial commit

---
+++ src/main/java/cl/ufro/proyectolp2/spring/data/Application.java
@@ -0,0 +1,13 @@
+package cl.ufro.proyectolp2.spring.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
