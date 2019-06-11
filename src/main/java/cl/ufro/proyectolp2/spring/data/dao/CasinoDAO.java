5fb7119a6e6e2574f8b59665af20098adc908874
PaLLoHoMe <49623698+PaLLoHoMe@users.noreply.github.com>
2019-06-11 17:46:28 -0400

    DAOS

---
+++ src/main/java/cl/ufro/proyectolp2/spring/data/dao/CasinoDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.ufro.proyectolp2.spring.data.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author pablo
+ */
+public interface CasinoDAO extends CrudRepository<Casino, Integer> {
+    
+}
