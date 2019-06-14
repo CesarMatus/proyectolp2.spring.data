95f845d0d820144908081a42abade7346260d674
CesarMatus <46377900+CesarMatus@users.noreply.github.com>
2019-06-14 18:57:28 -0400

    cambiando todo
    quedo la caga en la ayudantia y con la ayuda de los profes quedo algo como asi jajaja

--- src/main/java/cl/ufro/proyectolp2/spring/data/controlador/CasinoController.java
+++ src/main/java/cl/ufro/proyectolp2/spring/data/controlador/CasinoController.java
@@ -17,16 +17,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author cesar
  */
 @Controller
-@RequestMapping("casino")
+@RequestMapping("casinos")
 public class CasinoController {
     
     @Autowired
     private CasinoDAO casinoDAO; 
     
-    @GetMapping("/")
+    @GetMapping
     public String page(Model model) {
         
-        model.addAttribute("attribute", "value");
-        return "view.name";
+        model.addAttribute("casinos", this.casinoDAO.findAll());
+        return "casinos/index";
     }
 }
