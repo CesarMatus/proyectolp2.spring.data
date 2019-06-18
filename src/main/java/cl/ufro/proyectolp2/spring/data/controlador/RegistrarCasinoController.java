e8bfcbfd3f9977186a7702a085ab72c208dd198e
CesarMatus <46377900+CesarMatus@users.noreply.github.com>
2019-06-18 17:42:55 -0400

    arreglando vista casino
    no funciona jaja lol

---
+++ src/main/java/cl/ufro/proyectolp2/spring/data/controlador/RegistrarCasinoController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.ufro.proyectolp2.spring.data.controlador;
+
+import cl.ufro.proyectolp2.spring.data.dao.CasinoDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author cesar
+ */
+@Controller
+@RequestMapping("registrarcasino")
+public class RegistrarCasinoController {
+   
+    @Autowired
+    private CasinoDAO casinoDAO; 
+    
+    @GetMapping
+    public String page(Model model) {
+        
+        model.addAttribute("registrarcasino", this.casinoDAO.findAll());
+        return "casinos/registrarcasino";
+    }
+    
+    
+    
+    
+}
