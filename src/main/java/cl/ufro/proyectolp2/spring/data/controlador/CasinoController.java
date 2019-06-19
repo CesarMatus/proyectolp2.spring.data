0d350a3aa629c7bfe3931a2f3f93c59f3ab22752
CesarMatus <46377900+CesarMatus@users.noreply.github.com>
2019-06-19 16:00:45 -0400

    nueva ventana editar casino
    y tambien cambios en casino controller

--- src/main/java/cl/ufro/proyectolp2/spring/data/controlador/CasinoController.java
+++ src/main/java/cl/ufro/proyectolp2/spring/data/controlador/CasinoController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -47,4 +48,12 @@ public class CasinoController {
         this.casinoDAO.save(casino); 
         response.sendRedirect("");
     }
+    
+    @GetMapping("{id}")
+    public String show(@PathVariable("id") Integer id,Model model){
+        model.addAttribute("casinonuevo", casinoDAO.findById(id).get());
+        return "casinos/editarcasino"; 
+    }
+    
+    
 }
