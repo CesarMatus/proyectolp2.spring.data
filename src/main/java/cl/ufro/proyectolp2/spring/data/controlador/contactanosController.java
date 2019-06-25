75eee3f89735b0605eeac5318eaa4d2d949e3efb
CesarMatus <46377900+CesarMatus@users.noreply.github.com>
2019-06-25 18:51:32 -0400

    cambios con css en contacto

---
+++ src/main/java/cl/ufro/proyectolp2/spring/data/controlador/contactanosController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.ufro.proyectolp2.spring.data.controlador;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author cesar
+ */
+@Controller
+@RequestMapping("contacto")
+public class contactanosController {
+    
+    @GetMapping
+    public String page(Model model) {
+        return "contacto";
+    }
+    
+}
