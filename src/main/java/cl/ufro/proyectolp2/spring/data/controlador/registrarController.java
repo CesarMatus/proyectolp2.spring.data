47d10ced2a57489041a7b0f3abaa256b0c8a88b9
PaLLoHoMe <49623698+PaLLoHoMe@users.noreply.github.com>
2019-07-05 19:10:36 -0400

    arreglo del registrar D: y crea en la BD clientes.

--- src/main/java/cl/ufro/proyectolp2/spring/data/controlador/registrarController.java
+++ src/main/java/cl/ufro/proyectolp2/spring/data/controlador/registrarController.java
@@ -5,10 +5,16 @@
  */
 package cl.ufro.proyectolp2.spring.data.controlador;
 
+import cl.ufro.proyectolp2.spring.data.dao.ClienteDAO;
 import cl.ufro.proyectolp2.spring.data.modelo.Cliente;
+import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -19,10 +25,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping
 public class registrarController {
     
+    @Autowired
+    private ClienteDAO clienteDAO;
+    
    @GetMapping("registrar")
     public String page(Model model) {
         model.addAttribute("clientenuevo", new Cliente());
         return "registrar";
     }
     
+    @PostMapping("nuevoRegistro")
+    public void registrarClienteNuevo(@ModelAttribute Cliente cliente, HttpServletResponse response) throws IOException {
+        this.clienteDAO.save(cliente); 
+        response.sendRedirect("/");
+    }
 }
