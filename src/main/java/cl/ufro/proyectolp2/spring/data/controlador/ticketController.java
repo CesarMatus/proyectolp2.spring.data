90bdbfbd336be9ca3d371801b9fe790fb43e7bf3
CesarMatus <46377900+CesarMatus@users.noreply.github.com>
2019-07-09 04:08:41 -0400

    comenzando a hacer el ticket

---
+++ src/main/java/cl/ufro/proyectolp2/spring/data/controlador/ticketController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.ufro.proyectolp2.spring.data.controlador;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author cesar
+ */
+@Controller
+@RequestMapping("ticket")
+public class ticketController {
+    
+    @GetMapping
+    public String page(Model model) {
+        return "ticket";
+    }
+    
+}
